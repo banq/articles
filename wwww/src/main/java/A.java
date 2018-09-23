bbb8a2c6a462d478651aad53138d5f682e0c0357
banq <banq@163.com>
2018-09-23 15:24:21 +0800

    https://www.jdon.com/project/my-maven-repository-oss.html

---
+++ wwww/src/main/java/A.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+
+public class A {
+
+	private final int count;
+
+	public A(int count) {
+		this.count = count;
+	}
+
+	public int getCount() {
+		return count;
+	}
+}
+	
---
+++ wwww/src/main/java/SortedDemo.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class SortedDemo {
+
+	public static void main(String[] args) {
+		List<A> as = new ArrayList<A>();
+		as.add(new A(1));
+		as.add(new A(2));
+		as.add(new A(3));
+	}
+}
